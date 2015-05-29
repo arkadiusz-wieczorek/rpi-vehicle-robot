1e3969fd986263873b2455c7bed0049760f9dcff
Arkadiusz Wieczorek <ark.wieczorek@gmail.com>
2015-05-29 18:06:56 +0200

    Update init.js

--- init.js
+++ init.js
@@ -10,10 +10,6 @@ var GPIO = require('onoff').Gpio,
 	light_front_right = new GPIO(20, 'out'),
 	light_rear_left = new GPIO(19, 'out'),
 	light_rear_right = new GPIO(26, 'out'),
-    motor_down = new GPIO(22, 'out'), //down
-    motor_left  = new GPIO(23, 'out'), //left
-    motor_right = new GPIO(24, 'out'), //right
-    enable_1 = new GPIO(25, 'out'), //enable-1 L293DNE
     iv;
 
 motor[0] = motor_up;
